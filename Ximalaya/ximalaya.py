355d837eaa35c3ddf90a518b13adbba24500c84a
SalingerMa <ma_huimeng@163.com>
2019-03-04 15:51:49 +0800

    优化喜马拉雅音频下载器

--- Ximalaya/ximalaya.py
+++ Ximalaya/ximalaya.py
@@ -7,38 +7,57 @@ import re
 import os
 import sys
 from time import sleep
+import argparse
+import win32api, win32con
+
+class Common():
+    @classmethod
+    def get_desktop(self):
+        key = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER,
+                                  r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders', 0,
+                                  win32con.KEY_READ)
+        return win32api.RegQueryValueEx(key, 'Desktop')[0]
+    @classmethod
+    def make_dir(self, file_path):
+        result = '{}'
+        try:
+            if not os.path.exists(file_path):
+                result = "[新建文件夹] {}"
+                os.makedirs(file_path)
+            else:
+                result = '[文件夹已存在] {}'
+        except:
+            result = '[创建文件夹失败] {}'
+        finally:
+            print(result.format(file_path))
+
+class ArgParser():
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument('-i', '--xid', help='专辑ID', required=True)
+        self.parser.add_argument('-p', '--path', help='下载路径，默认下载到桌面')
+        self.args = self.parser.parse_args()
 
 class Ximalaya():
-    def __init__(self, id):
-        self.id = id
-        self.dpath = 'C:\work\pictures'
+    def __init__(self):
+        args = ArgParser().args
+        id_ = args.xid
+        path_ = args.path
+        self.id = id_
+        self.dpath = path_ if path_ != None else Common.get_desktop()
+        self.page = 1
         self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"}
         self.baseurl = "https://www.ximalaya.com/revision/play/album?albumId={}&pageNum={}"
-        self.page = 1
-        self.key = ''
+        self.key = True
         self.album = ''
-        self.get_m4a()
+        self.loop_one_code = True
 
-    def make_album_dir(self, file_name):
-        file_path = self.dpath + "/{}".format(file_name)
-        existed = os.path.exists(file_path)
-        if not existed:
-            print("新建文件夹：%s" % (file_name))
-            os.makedirs(file_path)
-        else:
-            pass
-
-    def test(self):
-        start_url = self.baseurl.format(self.id, self.page)
-        req = requests.get(start_url, headers=self.headers)
-        albumData = json.loads(req.content)
-        self.key = albumData['data']['hasMore']
-        self.album = albumData['data']["tracksAudioPlay"][0]['albumName']
-        return albumData['data']["tracksAudioPlay"]
-
-    def get_info(self, albumData):
-            for i in albumData:
-                yield {'name': i["trackName"], "url": i["src"]}
+    def get_json_data(self, url):
+        try:
+            req = requests.get(url, headers=self.headers)
+            return json.loads(req.content)
+        except:
+            print('requests请求失败')
 
     def download(self, name, url):
         _name = re.sub('\?|:|"|', "", name)
@@ -59,19 +78,29 @@ class Ximalaya():
         print()
 
     def get_m4a(self):
-        m4a_data = self.test()
-        self.make_album_dir(self.album)
-        for track in self.get_info(m4a_data):
-            sleep(1)
-            self.download(track['name'], track["url"])
-        if self.key == True:
+        _url = self.baseurl.format(self.id, self.page)
+        albumData = self.get_json_data(_url)
+        self.key = albumData['data']['hasMore']
+        m4a_data = albumData['data']["tracksAudioPlay"]
+
+        if self.loop_one_code:
+            self.album = m4a_data[0]['albumName']
+            file_path = self.dpath + "\%s" % self.album
+            Common.make_dir(file_path)
+            self.loop_one_code = False
+
+        for track in m4a_data:
+            sleep(0.5)
+            self.download(track["trackName"], track["src"])
+
+        if self.key:
             self.page += 1
             self.get_m4a()
-        else:
-            pass
 
 if __name__ == '__main__':
-    album_id = "11219907"
-    Ximalaya(album_id)
+    Ximalaya().get_m4a()
+    # python ximalaya.py -i 11219907 -p C:\Users\saler\Desktop --page 3
+
+
 
 
